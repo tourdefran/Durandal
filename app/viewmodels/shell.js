8729a72f048570c695cf38b6fcf624d28ba8110a
tourdefran <francoarias@gmail.com>
2015-02-24 17:17:28 -0300

    new menu item

--- app/viewmodels/shell.js
+++ app/viewmodels/shell.js
@@ -9,7 +9,8 @@
         activate: function () {
             router.map([
                 { route: '', title:'Welcome', moduleId: 'viewmodels/welcome', nav: true },
-                { route: 'flickr', moduleId: 'viewmodels/flickr', nav: true }
+                { route: 'flickr', moduleId: 'viewmodels/flickr', nav: true },
+                { route: 'test', moduleId: 'viewmodels/test', nav: true }
             ]).buildNavigationModel();
             
             return router.activate();
