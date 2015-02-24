0db71999590d2291f77b3f0ec3100578d2e878f2
tourdefran <francoarias@gmail.com>
2015-02-24 17:25:36 -0300

    new changes

--- app/viewmodels/test.js
+++ app/viewmodels/test.js
@@ -1,11 +1,9 @@
-define(function(require){
-	//require modules
-    var ko = require('knockout');
+define(['knockout','jquery'], function(ko, $){
     // dummy data for testing purposes
     var someValues = function(){
-        this.firstValue = 9;
-        this.secondValue = 21;
-        this.multipleValues = ['34','23','154','33'];
+        this.firstValue = ko.observable(9);
+        this.secondValue = ko.observable(21);
+        this.multipleValues = ko.observableArray(['34','23','154','33']);
     };
 
     return someValues;
